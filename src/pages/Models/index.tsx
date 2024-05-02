450f1141ec4cce2838805596d0a85ae23dc1f5c2
Satoshi Nakamoto <edtech1985@gmail.com>
2024-05-02 17:32:55 -0300

    scroll back to top

--- src/pages/Models/index.tsx
+++ src/pages/Models/index.tsx
@@ -28,6 +28,7 @@ import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import Fade from "@mui/material/Fade";
 
 import Snackbar, { SnackbarOrigin } from "@mui/material/Snackbar";
+import zIndex from "@mui/material/styles/zIndex";
 interface Model {
   id: number;
   name: string;
@@ -64,6 +65,43 @@ interface Filters {
 interface State extends SnackbarOrigin {
   open: boolean;
 }
+
+interface Props {
+  window?: () => Window;
+  children: React.ReactElement;
+}
+
+// === === === BEGIN SCROLL TO TOP === === === //
+function ScrollTop(props: Props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector("#back-to-top-anchor");
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box onClick={handleClick} role="presentation" sx={{ zIndex: 9999 }}>
+        {children}
+      </Box>
+    </Fade>
+  );
+}
+// === === === END SCROLL TO TOP === === === //
 
 export default function Models() {
   const [favorites, setFavorites] = useState<number[]>([]);
@@ -175,22 +213,9 @@ export default function Models() {
   });
   // === === === END FILTERING === === === //
 
-  // === === === BEGIN SCROLL TO TOP === === === //
-  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
-    const anchor = (
-      (event.target as HTMLDivElement).ownerDocument || document
-    ).querySelector("#back-to-top-anchor");
-
-    if (anchor) {
-      anchor.scrollIntoView({
-        block: "center",
-      });
-    }
-  };
-  // === === === END SCROLL TO TOP === === === //
-
   return (
     <Box textAlign="center">
+      <div id="back-to-top-anchor" />
       <Box mb={2}>
         <Typography variant="h4" component="h1" gutterBottom>
           Nossas Modelos
@@ -535,29 +560,27 @@ export default function Models() {
         ))}
       </Grid>
 
-      <Fade in={useScrollTrigger({ target: window })}>
-        <Box>
-          <Tooltip title="Voltar ao topo" arrow>
-            <Fab
-              size="small"
-              aria-label="scroll back to top"
-              sx={{
-                position: "fixed",
-                bottom: 16,
-                right: 16,
-                zIndex: 9999,
-                opacity: 0.4,
-                transition: "opacity 0.5s ease",
-                "&:hover": {
-                  opacity: 1,
-                },
-              }}
-            >
-              <KeyboardArrowUpIcon />
-            </Fab>
-          </Tooltip>
-        </Box>
-      </Fade>
+      <ScrollTop>
+        <Tooltip title="Voltar ao topo" arrow>
+          <Fab
+            size="small"
+            aria-label="scroll back to top"
+            sx={{
+              position: "fixed",
+              bottom: 16,
+              right: 16,
+              zIndex: 9999,
+              opacity: 0.4,
+              transition: "opacity 0.5s ease",
+              "&:hover": {
+                opacity: 1,
+              },
+            }}
+          >
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </Tooltip>
+      </ScrollTop>
     </Box>
   );
 }
